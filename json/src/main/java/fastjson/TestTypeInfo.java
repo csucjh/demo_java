e3f399cecb7fcbd20b91baaea4c2d6f71b3444bc
csucjh <jianhong.chen@56qq.com>
2018-08-16 15:50:14 +0800

    fastjson非全局ParserConfig

--- json/src/main/java/fastjson/TestTypeInfo.java
+++ json/src/main/java/fastjson/TestTypeInfo.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.Feature;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.util.IOUtils;
 import model.User;
 import org.springframework.web.servlet.FlashMap;
 
@@ -26,8 +27,32 @@ public class TestTypeInfo {
     }
 
     public static void main(String[] args) {
-        testUser();
+//        testUser();
 //        testMap();
+        testMapNewConfig();
+    }
+
+    /**
+     * 使用非全局的ParserConfig
+     */
+    public static void testMapNewConfig() {
+        // 使用非全局的ParserConfig
+        ParserConfig defaultRedisConfig = new ParserConfig();
+        defaultRedisConfig.setAutoTypeSupport(true);
+        defaultRedisConfig.addAccept("org.springframework.");
+        // 自定义类型信息key，默认@type
+        JSON.setDefaultTypeKey("@class");
+
+        Map<String, Object> user = new FlashMap();
+        user.put("name", "姓名");
+        user.put("age", "12");
+        user.put("features", Arrays.asList("aaaa", "bbbb", "ccc"));
+
+        byte[] bytes = JSON.toJSONBytes(user, SerializerFeature.WriteClassName);
+        System.out.println(bytes);
+
+        Object obj = JSON.parseObject(new String(bytes, IOUtils.UTF8), Object.class, defaultRedisConfig);
+        System.out.println(obj);
     }
 
     /**

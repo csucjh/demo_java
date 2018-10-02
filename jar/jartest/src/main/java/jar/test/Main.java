caabf63c4a5d9d4c6b85b1e5416d76ccf484f14e
csucjh <jianhong.chen@56qq.com>
2018-10-02 17:04:17 +0800

    打包jar

---
+++ jar/jartest/src/main/java/jar/test/Main.java
@@ -0,0 +1,16 @@
+package jar.test;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+public class Main {
+    public static void main(String[] args) {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("test");
+
+        Enumeration<String> enumerations = resourceBundle.getKeys();
+        while (enumerations.hasMoreElements()) {
+            String key = enumerations.nextElement();
+            System.out.println(key + "=" + resourceBundle.getString(key));
+        }
+    }
+}
